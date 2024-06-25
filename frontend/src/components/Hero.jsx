03ac5c0c23fa9f65a7f2963906592129dec871ec
hrsh_dev <139571547+hrshdev@users.noreply.github.com>
2024-06-25 15:16:41 +0530

    Add files via upload

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hero = ({ title, imageUrl }) => {
+  return (
+    <>
+      <div className="hero container">
+        <div className="banner">
+          <h1>{title}</h1>
+        </div>
+        <div className="banner">
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
