be8e2b0466245f783e889b86dc890f42374d4e08
madurangaPrabhath <maduracourse@gmail.com>
2024-06-29 09:56:55 +0530

    Edit Login

--- Dashboard/src/components/Login.jsx
+++ Dashboard/src/components/Login.jsx
@@ -45,7 +45,7 @@ const Login = () => {
   return (
     <>
       <section className="container form-component">
-        <img src="/logo.png" alt="logo" className="logo" />
+        <img src="/logo.png" alt="logo" className="logo"/>
         <h1 className="form-title">WELCOME TO VANNI HEALTHCARE</h1>
         <p>Only Admins are allowed to Access!</p>
         <form onSubmit={handleLogin}>
